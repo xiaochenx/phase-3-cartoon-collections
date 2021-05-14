889225df12393f6ec5dc8e4433549fa8d0459463
Xiaochen Xu <xiaochex@gmail.com>
2021-05-13 21:15:31 -0400

    commit tree 1a1b1a455e90e51540d596a2801d31429b2e2ac2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.each_with_index{ |name, index|
+
+  puts "#{index + 1}. #{name}"
+
+  }
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(heros)# code an argument here
   # Your code here
+  heros.map{|hero| hero.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(names)# code an argument here
   # Your code here
+  names.any? {|name| name.length > 4}
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find{|cheese| cheese_types.include?(cheese)}
+ 
 end
